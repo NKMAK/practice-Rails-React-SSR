df0d9c6ec8e6b0fcc52ba52b3722f237332fa152
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-06 19:30:45 +0900

    postテーブル作成（テスト）

---
+++ backend/db/migrate/20250206095431_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+    add_index :posts, [:title,:created_at], unique: true 
+  end
+end
