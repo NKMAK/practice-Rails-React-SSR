66a7c1450e19002a9ab7ea9f1db32078d492942a
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-07 15:44:00 +0900

    update schema

--- backend/db/migrate/20250207063442_create_refresh_tokens.rb
+++ backend/db/migrate/20250207063442_create_refresh_tokens.rb
@@ -1,14 +1,13 @@
 class CreateRefreshTokens < ActiveRecord::Migration[7.2]
   def change
     create_table :refresh_tokens do |t|
-        t.uuid :user_id, null: false
-        t.string :token, null: false
-        t.datetime :expires_at, null: false
-        t.timestamps
-      end
-      add_index :refresh_tokens, :user_id
-      add_index :refresh_tokens, :token, unique: true
-      add_foreign_key :refresh_tokens, :users
+      t.uuid :user_id, null: false
+      t.string :token, null: false
+      t.datetime :expires_at, null: false
+      t.timestamps
     end
+    add_index :refresh_tokens, :user_id
+    add_index :refresh_tokens, :token, unique: true
+    add_foreign_key :refresh_tokens, :users
   end
 end
