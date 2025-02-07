75059435c22197f47a5ce5d111e69b82618b0004
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-07 13:59:10 +0900

    ルーティングの記述方法変更

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  scope format: :json do
-    namespace :api do
-      resources :posts, only: [:create, :index]
-    end
+  namespace :api do
+    get "posts" => "posts#index"
+    post "posts" => "posts#create"
   end
+
 end
