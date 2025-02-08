c2a00f7748eabcc3a8939921ade4e1ddc715ea46
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-08 18:11:42 +0900

    refresh_token生成用のモジュール

--- backend/app/controllers/concerns/jwt_helper.rb
+++ backend/app/controllers/concerns/jwt_helper.rb
@@ -8,4 +8,23 @@ module JwtHelper
     }
     JWT.encode(payload, 'sample_secret_key', 'HS256')
   end
+
+  #try-catch入れたい
+
+  def create_refresh_token(user)
+    refresh_token_record = RefreshToken.create!(
+      user_id: user.id,
+      token: SecureRandom.hex(32),
+      expires_at: 30.days.from_now
+    )
+    refresh_token_record 
+  end
+
+  def update_refresh_token(found_refresh_token)
+    found_refresh_token.update!(
+      token: SecureRandom.hex(32),
+      expires_at: 30.days.from_now
+    )
+    found_refresh_token.reload
+  end
 end
