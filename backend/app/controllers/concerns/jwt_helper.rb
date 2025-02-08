30c0e07f044c6d6d6e4f4d7f5c6795ba0137e74b
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-08 17:37:29 +0900

    練習環境のためJWTのアルゴリズムと秘密鍵直接記述

--- backend/app/controllers/concerns/jwt_helper.rb
+++ backend/app/controllers/concerns/jwt_helper.rb
@@ -6,6 +6,6 @@ module JwtHelper
       user_id: user.uuid,
       exp: 24.hours.from_now.to_i
     }
-    JWT.encode(payload, Rails.application.credentials.jwt_secret)
+    JWT.encode(payload, 'sample_secret_key', 'HS256')
   end
 end
