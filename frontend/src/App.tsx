99b4f692043b732c22b76acf8da4f814ebebca87
clumsy-ug <rutobe740@gmail.com>
2025-02-14 18:44:03 +0900

    App.tsxもリファクタ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-interface ApiResponse {
-  id: number;
-  title: string;
-  content: string;
-  created_at: string;
-  updated_at: string;
-}
+import { Todo } from "./types";
+import { fetchTodos } from "./utils/fetchData";
 
 export default function App() {
-  const [posts, setPosts] = useState<ApiResponse[]>([]);
+  const [posts, setPosts] = useState<Todo[]>([]);
 
-  useEffect(() => {
+  if (!posts.length) {
     const initialFunc = async () => {
       try {
-        const response = await fetch("http://localhost:8000/api/posts");
-        // HTTPエラーをチェック (例: 404, 500)
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        console.log("responseは", response);
-        const result = await response.json();
-        console.log("resultは", result);
-        setPosts(result);
+        const todos = await fetchTodos("http://localhost:8000/api/posts");
+        console.log("todosは", todos);
+        setPosts(todos);
       } catch (e) {
         console.error("catchのeはこれ", e);
       }
     };
     initialFunc();
-  }, []);
+  }
 
   return (
     <>
